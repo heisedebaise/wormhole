ee221c0ce047fc8151269bd38368c007645d014b
heisedebaise <heisedebaise@hotmail.com>
2018-06-12 17:30:04 +0800

    输出日志到文件。

--- src/util/log.go
+++ src/util/log.go
@@ -2,8 +2,14 @@ package util
 
 import (
 	"log"
+	"os"
+	"time"
 )
 
 func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	os.MkdirAll("logs", os.ModePerm)
+	if file, err := os.OpenFile("logs/"+time.Now().Format("20060102"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666); err == nil {
+		log.SetOutput(file)
+	}
 }
